b82edb2115a16cae86c44d18d8449f80bb1f54ee
Alan Edwards <kaeatxnet@gmail.com>
2023-08-31 17:22:07 -0400

    works

--- src/components/RTabs/RTabs.vue
+++ src/components/RTabs/RTabs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="rvt-tabs r-tabs" :class="{ 'data-rvt-tabs': tabsLabel }">
-    <div class="rvt-tabs__tablist" role="tablist" aria-label="tabsAriaLabel">
+    <div class="rvt-tabs__tablist" role="tablist" :aria-label="tabsAriaLabel">
       <button
         v-for="(tabName, index) in tabNames"
         :key="'tab-' + index"
@@ -30,79 +30,42 @@
 </template>
 
 <script setup lang="ts">
-console.log('hello from RTabs component setup')
-
-import { computed, onMounted, onBeforeUnmount, ref, defineComponent } from "vue";
+import { computed, ref, onMounted, onBeforeUnmount } from "vue";
 import useModelWrapper from "../../utils/modelWrapper";
 
-export interface RTabsProps {
-  selectedTab?: number
-  tabNames?: string[]
-  tabsLabel?: string
-  tabsAriaLabel?: string
+interface RTabsProps {
+  selectedTab?: number;
+  tabNames?: string[];
+  tabsLabel?: string;
+  tabsAriaLabel?: string;
 }
 
-const props = withDefaults(defineProps<RTabsProps>(), {
+const props: RTabsProps = {
   selectedTab: 0,
   tabNames: ["Tab 1", "Tab 2", "Tab 3", "Tab 4"],
   tabsLabel: "tabset-1",
   tabsAriaLabel: "Tabs",
+};
+
+const emit = useModelWrapper(props, name => `update:${name}`);
+
+const selectedTabValue = ref(props.selectedTab);
+const tabNames = computed(() => props.tabNames);
+const tabsLabel = computed(() => props.tabsLabel);
+const tabsAriaLabel = computed(() => props.tabsAriaLabel);
+
+function tabActivated(event: any): void {
+  const parsedTabIndex = Number(event.detail.name.split("-")[1]);
+  if (!Number.isNaN(parsedTabIndex)) {
+    selectedTabValue.value = parsedTabIndex;
+  }
+}
+
+onMounted(() => {
+  document.addEventListener("rvtTabActivated", tabActivated);
 });
 
-// const slots = defineSlots({
-//   default: () => ["Panel Information for Tab 1", "Panel Information for Tab 2", "Panel Information for Tab 3", "Panel Information for Tab 4"],
-// });
-
-const RTabs = defineComponent({
-  props: {
-    selectedTab: {
-      type: Number,
-      default: 0,
-    },
-    tabNames: {
-      type: Array,
-      default: () => ["Tab 1", "Tab 2", "Tab 3", "Tab 4"],
-    },
-    tabsLabel: {
-      type: String,
-      default: "tabset-1",
-    },
-    tabsAriaLabel: {
-      type: String,
-      default: "Tabs",
-    },
-  },
-  setup(props: RTabsProps) {
-    const selectedTabValue = ref(props.selectedTab);
-    const tabNames = computed(() => props.tabNames);
-    const tabsLabel = computed(() => props.tabsLabel);
-    const tabsAriaLabel = computed(() => props.tabsAriaLabel);
-
-    function tabActivated(event: any): void {
-      let parsedTabIndex = Number(event.detail.name.split("-")[1]);
-      if (!Number.isNaN(parsedTabIndex)) {
-        selectedTabValue.value = parsedTabIndex;
-      }
-    }
-
-    // Use the emit function directly
-    const emit = useModelWrapper(props, name => `update:${name}`);
-
-    onMounted(() => {
-      document.addEventListener("rvtTabActivated", tabActivated);
-    });
-
-    onBeforeUnmount(() => {
-      document.removeEventListener("rvtTabActivated", tabActivated);
-    });
-
-    return {
-      selectedTabValue,
-      tabNames,
-      tabsLabel,
-      tabsAriaLabel,
-      emit, // Expose emit for template
-    };
-  },
+onBeforeUnmount(() => {
+  document.removeEventListener("rvtTabActivated", tabActivated);
 });
 </script>
