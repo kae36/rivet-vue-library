08e59d4f175e42797cb7189f09798fc12b9cbca0
Alan Edwards <keaedwar@iu.edu>
2023-09-01 17:07:46 -0400

    working on Icons

--- wallaby.js
+++ wallaby.js
@@ -2,7 +2,12 @@ process.env.NODE_ENV = "test"
 module.exports = function (wallaby) {
   return {
     autoDetect: true,
-    tests: ["tests/**/*.spec.js", "tests/**/*.spec.ts", "src/**/*.spec.js", "src/**/*.spec.ts"]
+    tests: [
+      "tests/**/*.spec.js",
+      "tests/**/*.spec.ts",
+      "src/**/*.spec.js",
+      "src/**/*.spec.ts"
+    ]
     // for node.js tests you need to set env property as well
     // https://wallabyjs.com/docs/integration/node.html
   }
